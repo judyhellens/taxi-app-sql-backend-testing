2826c7aa59070e5e166836c789c9fa7ab65b565c
Judy Hellen Silva <judyhellenss@hotmail.com>
2026-05-24 22:19:54 +0100

    Update taxi_company_analysis.sql

--- sql_queries/taxi_company_analysis.sql
+++ sql_queries/taxi_company_analysis.sql
@@ -1,1 +1,10 @@
-
+SELECT
+    company_name,
+    COUNT(*) AS trips_amount
+FROM cabs
+INNER JOIN trips
+ON cabs.cab_id = trips.cab_id
+WHERE start_ts BETWEEN '2017-11-15'
+AND '2017-11-16'
+GROUP BY company_name
+ORDER BY trips_amount DESC;
